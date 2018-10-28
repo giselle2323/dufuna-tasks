5ae62ebfbd97c0a956ea10eba68278f74d88a8cc
yakubu aminat jumoke <38222819+giselle2323@users.noreply.github.com>
2018-10-28 01:38:48 +0100

    add files via upload

--- signup/data.sql
+++ signup/data.sql
@@ -2,12 +2,12 @@ CREATE DATABASE registration;
 USE registration; 
 CREATE TABLE users ( 
 id INT NOT NULL AUTO_INCREMENT, 
-Firstname VARCHAR(150) NOT NULL, 
-Lastname VARCHAR(150) NOT NULL,
+firstname VARCHAR(150) NOT NULL, 
+lastname VARCHAR(150) NOT NULL,
 email VARCHAR(150) NOT NULL,
  `password` VARCHAR(17) NOT NULL,
-`confirm password` VARCHAR(17) NOT NULL,
-telephone TINYINT (15),
+`confirmpassword` VARCHAR(17) NOT NULL,
+telephone VARCHAR(19),
 gender VARCHAR (9),
 country VARCHAR (40),
 created_date DATETIME, 
--- signup/process.php
+++ signup/process.php
@@ -1,23 +1,26 @@
 <?php 
-$fname = $_POST['Firstname'];
-$lname = $_POST['Lastname']; 
+$fname = $_POST['firstname'];
+$lname = $_POST['lastname']; 
 $email = htmlspecialchars($_POST['email']);
 $psw = $_POST['password'];
-$cpsw = $_POST['confirm password'];
+$cpsw = $_POST['confirmpassword'];
 $tele = $_POST['telephone'];
 $gender = $_POST['gender'];
 $country = $_POST['country'];
 saveToFile($fname, $lname,  $email, $psw, $cpsw, $tele, 
 	$gender, $country); 
+saveToDatabase($fname, $lname,  $email, $psw, $cpsw, $tele, 
+	$gender, $country);
 header('Location:success.html');
 
-function saveToFile($fname, $lname, $email, $psw, $cpsw, $tele, 
-	$gender, $country) {   
+function saveToFile($fname, $lname, $email, $psw, $cpsw, 
+	$tele, $gender, $country) {   
 	$fileHandler = fopen('record.txt', 'a');   
 	$string = $fname . ',' . $lname . ',' . $email. ',' . $psw . ',' . $cpsw . ',' . $tele . ',' . $gender . ',' . $country . "\n";   
 	fwrite($fileHandler, $string);   
 	fclose($fileHandler); 
 }
+
 function saveToDatabase($fname, $lname, $email, $psw, $cpsw, $tele, $gender, $country ) {   
 $serverName = "localhost";   
 $database = "registration";   
@@ -27,9 +30,9 @@ $password = "";
 $conn = mysqli_connect($serverName, $username, $password, $database);
    // Check that connection exists   
 if (!$conn) {       die("Connection failed: " . mysqli_connect_error());   }     
-$sql = "INSERT INTO users (Firstname, Lastname, email, password, confirm passwod, telephone, gender,country,created_date)       
+$sql = "INSERT INTO users (firstname, lastname, email, password, confirmpassword, telephone, gender,country,created_date)       
 VALUES ('$fname', '$lname', '$email', '$psw', '$cpsw', 
-'$tele', '$gender', '$country' NOW())";   
+'$tele', '$gender', '$country', NOW())";   
 	$result = mysqli_query($conn, $sql);
    //Check for errors   
    if (!$result) {       die("Error: " . $sql . "<br>" . mysqli_error($conn));   }   
