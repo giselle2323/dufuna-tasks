7bf6706b70e64cea42af86e0994805bb5b19974a
yakubu aminat jumoke <38222819+giselle2323@users.noreply.github.com>
2018-10-31 13:00:39 +0100

    add files via upload

--- signup/process.php
+++ signup/process.php
@@ -22,21 +22,21 @@ function saveToFile($fname, $lname, $email, $psw, $cpsw,
 }
 
 function saveToDatabase($fname, $lname, $email, $psw, $cpsw, $tele, $gender, $country ) {   
-$serverName = "localhost";   
-$database = "registration";   
-$username = "root";   
-$password = "";
-   //Open database connection   
-$conn = mysqli_connect($serverName, $username, $password, $database);
-   // Check that connection exists   
-if (!$conn) {       die("Connection failed: " . mysqli_connect_error());   }     
-$sql = "INSERT INTO users (firstname, lastname, email, password, confirmpassword, telephone, gender,country,created_date)       
-VALUES ('$fname', '$lname', '$email', '$psw', '$cpsw', 
-'$tele', '$gender', '$country', NOW())";   
+    $serverName = "localhost";   
+    $database = "registration";   
+    $username = "root";   
+    $password = "";
+    //Open database connection   
+    $conn = mysqli_connect($serverName, $username, $password, $database);
+    // Check that connection exists   
+    if (!$conn) {       die("Connection failed: " . mysqli_connect_error());   }     
+    $sql = "INSERT INTO users (firstname, lastname, email, password, confirmpassword, telephone, gender,country,created_date)       
+    VALUES ('$fname', '$lname', '$email', '$psw', '$cpsw', 
+    '$tele', '$gender', '$country', NOW())";   
 	$result = mysqli_query($conn, $sql);
-   //Check for errors   
-   if (!$result) {       die("Error: " . $sql . "<br>" . mysqli_error($conn));   }   
-   //Close the connection   
-   mysqli_close($conn); 
+    //Check for errors   
+    if (!$result) {       die("Error: " . $sql . "<br>" . mysqli_error($conn));   }   
+    //Close the connection   
+    mysqli_close($conn); 
 }
 ?>
