d5b689762e7ce37899895113e77a4a7490b11805
yakubu aminat jumoke <38222819+giselle2323@users.noreply.github.com>
2018-11-06 18:06:53 +0100

    add files via upload

--- wishlist/wishlist.js
+++ wishlist/wishlist.js
@@ -2,7 +2,9 @@ $(document).ready(function(){ //Runs script after page loads.
 	$('#add').click(function(e){ //When button is clicked,execute the event below
 	    var wish =$('#textfield').val(); //A variable "wish" is assigned to the value entered in the textfield.
 	    $('#list'). append('<li>'+wish+'</li>');// select id #list and append variable"wish" into the list tags.
-		e.preventDefault();// Prevents holding default value in the textfield.
+		$('#textfield').val(null);
+		e.preventDefault();
+		// Prevents holding default value in the textfield.
 	});
 });
 
