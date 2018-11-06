fd3dcbd64346a9376b0aec4a5eb629889ee0d683
yakubu aminat jumoke <38222819+giselle2323@users.noreply.github.com>
2018-11-06 20:19:54 +0100

    add files via upload

---
+++ form validation/formvalidation.js
@@ -0,0 +1,64 @@
+  function validateForm() {           
+  
+  var firstname = document.signupForm.firstname;
+  if (firstname.value == "") {               
+  alert("Please input your first name");               
+  firstname.focus();               
+  return false;           
+  }
+  var lastname = document.signupForm.lastname;           
+  if (lastname.value == "") {               
+  alert("Please input your last name");               
+  lastname.focus();               
+  return false;           
+  }
+  var email = document.forms["signupForm"]["email"];           
+  if (email.value == "") {               
+  	alert("Please input your email address");               
+  email.focus();             
+  return false; 
+  }
+  var password = document.signupForm.password;           
+  if (password.value == "") {               
+  alert("Please input your password");               
+  password.focus();               
+  return false;           
+  }
+  if (password.value.length != 11) {
+       alert( "password should be exactly 11 characters.");              
+        password.focus();               
+       return false;
+   }
+  var confirmpassword = document.signupForm.confirmpassword;           
+  if (confirmpassword.value == "") {               
+  alert("Please input your password");               
+  confirmpassword.focus();               
+  return false;           
+  }
+  if (confirmpassword.value.length != 11) {
+       alert( "password should be exactly 11 characters.");              
+        confirmpassword.focus();               
+	       return false;  
+	   }  
+  var telephone = document.signupForm.telephone           
+  if (telehone.value == "" || isNaN(telephone.value)) {  
+  	 alert("Phone number should be numeric.");               
+  	 telephone.focus();              
+  	  return false;           
+  	}
+  if (telephone.value.length != 11) {
+       alert( "Phone number should be exactly 11 digits.");              
+        telephone.focus();               
+       return false;
+   }
+    var gender = document.signupForm.gender;           
+  if (gender.value == "") {               
+  alert("Please input your last name");               
+  gender.focus();               
+  return false;           
+  }
+  var country = document.signupForm.country;           
+  if (country.value == "-1") {               
+  	alert("Please select your country");               
+  	return false;           }          
+  	 return true;       } 
