8ea8182721a6491642b7a4590257f9aabf60d073
yakubu aminat jumoke <38222819+giselle2323@users.noreply.github.com>
2018-11-12 10:29:05 +0100

    add files via upload

--- severside validation/process.php
+++ severside validation/process.php
@@ -1,31 +1,48 @@
 <?php
  $message = ''; 
- if (isset($_POST['submit'])) {    
- $firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];    
-$email = $_POST['email'];
-$password = $_POST['password'];
-$confirmpassword = $_POST['confirmpassword'];    
-$telephone = $_POST['telephone'];
-$gender = $_POST['gender'];    
-$country = $_POST['country'];
-    if (empty($firstname) || empty($lastname) || empty($email) || empty($password) || empty($confirmpassword) || empty($telephone) || empty($gender) || empty($country)) {       
-     $message = '<p class="error">All fields are required</p>';    
-     }   
-     } 
-     if (strlen($firstname) < 3 || strlen($firstname) > 20) {    
-     $message .= '<p class="error">Full name must be between 3 and 20 characters</p>'; }
-     if (!is_numeric($telephone)) {     
-     $message .= '<p class="error">Phone number should be numeric</p>'; }
-      if (empty($message)) {            
-      $message = '<p class="success">All inputs are valid, thank you</p>';       }
-
-
-     ?> 
-
-
+ if (isset($_POST['submit'])) 
+ {    
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];    
+    $email = $_POST['email']; 
+    $password = $_POST['password'];
+    $confirmpassword = $_POST['confirmpassword'];    
+    $telephone = $_POST['telephone'];
+    $gender = $_POST['gender'];    
+    $country = $_POST['country'];
+    if (empty($firstname) || empty($lastname) || empty($email) || empty($password) || empty($confirmpassword) || 
+    empty($telephone)|| empty($gender) || empty($country)) 
+    {       
+      $message = '<p class="error">All fields are required</p>';    
+    }   
+  } 
+  if (strlen($firstname) < 3 || strlen($firstname) > 20) 
+  {    
+    $message .= '<p class="error">Firstname must be between 3 and 20 characters</p>'; 
+  }
+    if (strlen($lastname) < 3 || strlen($lastname) > 20) 
+  {    
+    $message .= '<p class="error">lastname must be between 3 and 20 characters</p>'; 
+  }
+  if ($_POST["password"]===$_POST["confirm_password"])
+  {
+    //success!
+  }
+  else
+  {
+    //failed:(
+  }
+  if (!is_numeric($telephone)) 
+  {     
+    $message .= '<p class="error">Phone number should be numeric</p>'; 
+  }
+    if (empty($message)) 
+  {            
+    $message = '<p class="success">All inputs are valid, thank you</p>';       
+  }
+?> 
 <!DOCTYPE html>
-<html>
+  <html>
     <head>
         <title>MY form example</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -45,15 +62,15 @@ $country = $_POST['country'];
               PLEASE SIGN UP HERE
             </h1>
             <?php echo $message; ?>
-           <form method="POST" name="signupForm" action="">
+           <form method="POST" name="signupForm" action="upload.php">
                 <fieldset>
                     <legend> Sign Up:</legend>
                       <label for="firstname">firstname:
-                      <input type="text" name="firstname" id="name">
+                      <input type="text" name="firstname" id="fname">
                     </label>
                     <br>
                     <label for="lastname" >lastname:
-                      <input type="text" name="lastname" id="name">
+                      <input type="text" name="lastname" id="lname">
                    </label>
                    <br>
                    <label for="email">Email:
@@ -61,7 +78,7 @@ $country = $_POST['country'];
                     </label>
                     <br>
                     <label for="password">Password:
-                      <input type="password" name="password" id="password">
+                       <input type="password" name="password" id="password">
                     </label>
                     <br>
                     <label for="confirmpassword"> Confirm Password:
@@ -69,7 +86,7 @@ $country = $_POST['country'];
                     </label>
                     <br>
                     <label for="phone number"> Phone Number:
-                      <input type="number" name="telephone" id="telephone">
+                      <input type="text" name="telephone" id="telephone">
                     </label>
                     <br>
                     <label > Gender: </label>
---
+++ severside validation/upload.php
@@ -0,0 +1,51 @@
+<?php 
+$fname = $_POST['firstname'];
+$lname = $_POST['lastname']; 
+$email = htmlspecialchars($_POST['email']);
+$psw = $_POST['password'];
+$cpsw = $_POST['confirmpassword'];
+$tele = $_POST['telephone'];
+$gender = $_POST['gender'];
+$country = $_POST['country'];
+saveToFile($fname, $lname,  $email, $psw, $cpsw, $tele, 
+	$gender, $country); 
+saveToDatabase($fname, $lname,  $email, $psw, $cpsw, $tele, 
+	$gender, $country);
+header('Location:success.html');
+
+function saveToFile($fname, $lname, $email, $psw, $cpsw, 
+	$tele, $gender, $country) {   
+	$fileHandler = fopen('record.txt', 'a');   
+	$string = $fname . ',' . $lname . ',' . $email. ',' . $psw . ',' . $cpsw . ',' . $tele . ',' . $gender . ',' . $country . "\n";   
+	fwrite($fileHandler, $string);   
+	fclose($fileHandler); 
+}
+
+function saveToDatabase($fname, $lname, $email, $psw, $cpsw, $tele, $gender, $country ) 
+    {   
+        $serverName = "localhost";   
+        $database = "registration";   
+        $username = "root";   
+        $password = "";
+        //Open database connection   
+        $conn = mysqli_connect($serverName, $username, $password, $database);
+        // Check that connection exists   
+        if (!$conn) 
+        {
+           die("Connection failed: " . mysqli_connect_error());   
+        }     
+        $sql = "INSERT INTO users (firstname, lastname, email, password, confirmpassword, telephone, 
+        gender,country,created_date)       
+        VALUES ('$fname', '$lname', '$email', '$psw', '$cpsw', 
+        '$tele', '$gender', '$country', NOW())";
+        if (mysqli_query($conn, $sql)) 
+        {
+            echo "New record created successfully";
+        } 
+        else 
+        {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+        mysqli_close($conn);
+    } 
+?>
